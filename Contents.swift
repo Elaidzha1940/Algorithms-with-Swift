8b576fadb017aebb067e55c5e253f42c84de509d
Elaidzha1940 <eli080293@gmail.com>
2024-09-30 19:19:47 +0300

    Tree / TreeNode / Try - struct Product

--- Contents.swift
+++ Contents.swift
@@ -1,6 +1,14 @@
 import UIKit
 
+// Queue
+
+struct Queue<T> {
+    
+}
+
 // "Tree"
+
+// Node
 class TreeNode<T> {
     var value: T
     var children: [TreeNode] = []
@@ -16,8 +24,35 @@ class TreeNode<T> {
 
 extension TreeNode {
     func forEach(visit: (TreeNode) -> ()) {
-        children.forEach { treeNode in
-            treeNode.forEach(visit: visit)
-        }
+        visit(self)
+        children.forEach { $0.forEach(visit: visit) }
     }
 }
+
+struct Product {
+    let title: String
+    let category: String
+    let subCategory: String
+}
+
+let productsTree = TreeNode(value: "Catalog")
+let shoesTree = TreeNode(value: "Shoes")
+let womenShoes = TreeNode(value: "Women Shoes")
+let menShoes = TreeNode(value: "Men Shoes")
+let sneakers = TreeNode(value: "Sneakers")
+let oxfords = TreeNode(value: "Oxfords")
+let slippers = TreeNode(value: "Slippers")
+let crocs = TreeNode(value: "Crocs")
+
+productsTree.add(shoesTree)
+shoesTree.add(womenShoes)
+shoesTree.add(menShoes)
+womenShoes.add(slippers)
+womenShoes.add(crocs)
+menShoes.add(sneakers)
+menShoes.add(oxfords)
+
+productsTree.forEach { node in
+    print(node.value)
+}
+
