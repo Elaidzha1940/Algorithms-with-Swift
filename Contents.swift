fa4f257587ddab0034816d1fc18effd6452e3fbc
Elaidzha1940 <eli080293@gmail.com>
2024-09-30 17:37:47 +0300

    Tree / TreeNode

--- Contents.swift
+++ Contents.swift
@@ -1,5 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// "Tree"
+class TreeNode<T> {
+    var value: T
+    var children: [TreeNode] = []
+    
+    init(value: T) {
+        self.value = value
+    }
+    
+    func add(_ child: TreeNode) {
+        children.append(child)
+    }
+}
 
-
+extension TreeNode {
+    func forEach(visit: (TreeNode) -> ()) {
+        children.forEach { treeNode in
+            treeNode.forEach(visit: visit)
+        }
+    }
+}
