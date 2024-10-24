1c1877e29ba4031dbf7c170568e954365de0cd50
Elaidzha1940 <eli080293@gmail.com>
2024-10-24 16:18:28 +0300

    Add Stack / Tested

--- Stack.playground/Contents.swift
+++ Stack.playground/Contents.swift
@@ -5,5 +5,41 @@ struct Stack<T> {
     var size: Int { container.count } // Size
     var isEmpty: Bool { container.isEmpty } // Empty
     var top: T? { container.last } // Top Element
+    
+    mutating func push(_ value : T) {
+        self.container.append(value)
+    }
+    
+    mutating func pop() -> T? {
+        guard !self.isEmpty else {
+            return nil
+        }
+        return self.container.removeLast()
+    }
 }
+
+extension Stack {
+    mutating func push(_ array: [T]) {
+        self.container += array
+    }
+    
+    mutating func removeAll() {
+        container.removeAll()
+    }
+}
+
+// Test
+var stack = Stack<String>()
+stack.pop()
+
+stack.push("Autmn")
+stack.push("Winter")
+stack.push("Spring")
+stack.push("Summer")
+
+stack.pop()
+stack.isEmpty
+stack.top
+stack.size
+
 
