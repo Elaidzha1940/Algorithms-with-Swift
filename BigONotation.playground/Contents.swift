62fedfcd7d94ae2a000fbdf2e168fdf1531baa27
Elaidzha1940 <eli080293@gmail.com>
2024-10-28 14:36:04 +0300

    O(n) / O(2*n)

--- BigONotation.playground/Contents.swift
+++ BigONotation.playground/Contents.swift
@@ -6,12 +6,41 @@ import UIKit
 // O — количество операций, которое потребуется для выполнения алгоритма.
 // n — количество элементов, с которыми работает алгоритм.
 
-func getValueFor(index: Int, in array: [Int]) -> Int? {
+// O(1) - Единичная (константная) сложность
+
+func getSquareFor(index: Int, in array: [Int]) -> Int? {
     guard index < array.count else { return nil }
-    
-    return array[index]
+    let value = array[index]
+    return value * value
 }
 
-getValueFor(index: 2, in: [1,2,3,4,5])
+//getSquareFor(index: 2, in: [1,2,3,4,5])
 
+// O(n) - Линейная сложность
 
+func sum(array: [Int]) -> Int {
+    var sum = 0
+    array.forEach { sum += $0 }
+    return sum
+    
+    //    for num in array {
+    //        sum += num
+    //    }
+    
+    //    array.forEach { num in
+    //        sum += num
+    //    }
+}
+
+// O(2*n)
+func sum(array: [Int]) -> (Int, Int) {
+    var sum = 0
+    var mult = 1
+    array.forEach { sum += $0 }
+    
+    array.forEach { num in
+        mult *= num
+    }
+    
+    return (sum, mult)
+}
